f90e5703c10a544cc85d42ba64ae4363f87d03f8
nbnurlan <125850954+nbnurlan@users.noreply.github.com>
2026-09-14 23:32:47 +0500

    Remove KSP dependencies from build.gradle.kts
    Removed KSP dependencies for Room and Moshi.

--- build.gradle.kts
+++ build.gradle.kts
@@ -132,6 +132,4 @@ dependencies {
   androidTestImplementation(libs.androidx.runner)
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
-  "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
 }
